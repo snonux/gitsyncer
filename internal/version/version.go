50d51cd51975360f7a81b6f1251de184dcc27edd
Paul Buetow <paul@buetow.org>
2025-07-25 00:21:02 +0300

    chore(release): bump version to 0.8.5

--- internal/version/version.go
+++ internal/version/version.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	// Version is the current version of gitsyncer
-	Version = "0.8.4"
+	Version = "0.8.5"
 
 	// GitCommit is the git commit hash at build time
 	GitCommit = "unknown"
