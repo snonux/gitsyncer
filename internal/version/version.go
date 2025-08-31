5b11109180238909da3fbf7e391d67d7d9851913
Paul Buetow <paul@buetow.org>
2025-08-31 23:00:48 +0300

    chore(version): bump to 0.9.1

--- internal/version/version.go
+++ internal/version/version.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	// Version is the current version of gitsyncer
-    Version = "0.9.0"
+    Version = "0.9.1"
 
 	// GitCommit is the git commit hash at build time
 	GitCommit = "unknown"
